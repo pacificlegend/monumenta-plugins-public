b537c9204793dfc6116461fc3f116d4137af6c26
Timothy Southwick <NickNackGus@gmail.com>
2026-04-14 16:05:20 -0400

    Fix /offduty for moderators (#6196)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/integrations/luckperms/OffDutyCommand.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/integrations/luckperms/OffDutyCommand.java
@@ -47,11 +47,6 @@ public class OffDutyCommand extends TogglePermCommand {
 		return true;
 	}
 
-	@Override
-	public boolean assumeDefaultDisabled() {
-		return true;
-	}
-
 	public static void register() {
 		new OffDutyCommand().registerCommand();
 	}
